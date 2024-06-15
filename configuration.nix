b610f799bea93d005420ade2880e84da92dea860
Christian Hagenest <mail@hagenest.dev>
2024-06-15 17:01:56 +0200

    nixfmt

--- configuration.nix
+++ configuration.nix
@@ -35,26 +35,27 @@
       };
     };
 
-    services.prometheus.exporters.node = {
+  services.prometheus.exporters.node = {
     enable = true;
     port = 9000;
     # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
     enabledCollectors = [ "systemd" ];
     # /nix/store/zgsw0yx18v10xa58psanfabmg95nl2bb-node_exporter-1.8.1/bin/node_exporter  --help
-    extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
+    extraFlags =
+      [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
   };
 
   services.prometheus = {
     enable = true;
     globalConfig.scrape_interval = "3s"; # "1m"
-    scrapeConfigs = [
-    {
+    scrapeConfigs = [{
       job_name = "node";
       static_configs = [{
-        targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        targets = [
+          "localhost:${toString config.services.prometheus.exporters.node.port}"
+        ];
       }];
-    }
-    ];
+    }];
   };
 
   services.invidious = {
@@ -73,6 +74,21 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 80 443 3001 9000 ];
+  };
+
+  # auto-update and garbage collection
+  system.autoUpgrade = {
+    enable = true;
+    dates = "*-*-* 04:00:00";
+    persistent = true;
+    allowReboot = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    persistent = false;
+    dates = "daily";
+    options = "--delete-older-than 30d";
   };
 
   boot.tmp.cleanOnBoot = true;
--- networking.nix
+++ networking.nix
@@ -2,10 +2,8 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "2a01:4ff:ff00::add:1"
- "2a01:4ff:ff00::add:2"
- "185.12.64.2"
- ];
+    nameservers =
+      [ "2a01:4ff:ff00::add:1" "2a01:4ff:ff00::add:2" "185.12.64.2" ];
     defaultGateway = "172.31.1.1";
     defaultGateway6 = {
       address = "fe80::1";
@@ -15,21 +13,34 @@
     usePredictableInterfaceNames = lib.mkForce false;
     interfaces = {
       eth0 = {
-        ipv4.addresses = [
-          { address="128.140.36.120"; prefixLength=32; }
-        ];
+        ipv4.addresses = [{
+          address = "128.140.36.120";
+          prefixLength = 32;
+        }];
         ipv6.addresses = [
-          { address="2a01:4f8:1c1b:7ea4::1"; prefixLength=64; }
-{ address="fe80::9400:3ff:fe64:b351"; prefixLength=64; }
+          {
+            address = "2a01:4f8:1c1b:7ea4::1";
+            prefixLength = 64;
+          }
+          {
+            address = "fe80::9400:3ff:fe64:b351";
+            prefixLength = 64;
+          }
         ];
-        ipv4.routes = [ { address = "172.31.1.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
+        ipv4.routes = [{
+          address = "172.31.1.1";
+          prefixLength = 32;
+        }];
+        ipv6.routes = [{
+          address = "fe80::1";
+          prefixLength = 128;
+        }];
       };
-      
+
     };
   };
   services.udev.extraRules = ''
     ATTR{address}=="96:00:03:64:b3:51", NAME="eth0"
-    
+
   '';
 }
